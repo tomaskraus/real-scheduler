f940d1e0ab1dbcfa0090cf6cba4ced85f00850e9
tomaskraus <tomas.krausik@gmail.com>
2017-06-20 00:34:46 +0200

    better comments in example

--- example.js
+++ example.js
@@ -7,9 +7,12 @@ var RealScheduler = require('./src/RealScheduler');
  * @delay <number> The number of milliseconds to wait between callbacks
  */
 var scheduler = new RealScheduler((sch) => {
-        // how many times this callback was called since the scheduler's start
-        console.log(sch.getTimeElapsed() + ": Call count: " + sch.getNumberOfCalls());
-        // a real, accumulated time since the scheduler start
+
+        console.log(
+            sch.getTimeElapsed() +   // a real, accumulated time since the scheduler start
+            ": Call count: " + sch.getNumberOfCalls()  // how many times this callback was called since the scheduler's start
+        );
+
         if (sch.getNumberOfCalls() == 60) {
             sch.stop(); //stop the scheduler
             console.log("execution stopped");
