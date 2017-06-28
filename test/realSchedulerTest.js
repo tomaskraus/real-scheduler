a024e088031f7f001710f7460434c643dc45f96f
tomaskraus <tomas.krausik@gmail.com>
2017-06-28 14:47:03 +0200

    tests with coverage report

--- test/realSchedulerTest.js
+++ test/realSchedulerTest.js
@@ -2,4 +2,35 @@ var tap = require('tap');
 
 var RealScheduler = require('../src/RealScheduler');
 
-tap.pass('this is fine');
+
+
+tap.test('1st call returns syntheticTime equals to delay', function (childTest) {
+  var scheduler = new RealScheduler((sch) => {
+
+        sch.stop();
+        childTest.equal(sch.getSyntheticTimeElapsed(), 100);
+        childTest.end();
+    }, 100);
+});
+
+tap.test('1st call (with waitForTheFirstCall option false) returns syntheticTime equals to 0', function (childTest) {
+  var scheduler = new RealScheduler((sch) => {
+
+        sch.stop();
+        childTest.equal(sch.getSyntheticTimeElapsed(), 0);
+        childTest.equal(sch.getTimeElapsed(), 0);
+        childTest.end();
+    }, 100, {waitForTheFirstCall: false});
+});
+
+// tap.test('1st call (with waitForTheFirstCall option false) returns syntheticTime equals to 0', function (childTest) {
+//   var scheduler = new RealScheduler((sch) => {
+
+//         sch.stop();
+//         childTest.equal(sch.getSyntheticTimeElapsed(), 0);
+//         childTest.equal(sch.getTimeElapsed(), 0);
+//         childTest.end();
+//     }, 100, {waitForTheFirstCall: false});
+// });
+
+
