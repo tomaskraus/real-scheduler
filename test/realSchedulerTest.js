59802c52cf6379901913ef5135073e72e5bddac0
tomaskraus <tomas.krausik@gmail.com>
2017-06-28 16:03:07 +0200

    Mocha BDD-style tap tests

--- test/realSchedulerTest.js
+++ test/realSchedulerTest.js
@@ -1,36 +1,30 @@
-var tap = require('tap');
+require('tap').mochaGlobals();
+var assert = require('assert');
+
 
 var RealScheduler = require('../src/RealScheduler');
 
 
 
-tap.test('1st call returns syntheticTime equals to delay', function (childTest) {
-  var scheduler = new RealScheduler((sch) => {
+describe('RealScheduler', function() {
+    it('1st call returns syntheticTime equals to delay', function(done) {
+        var scheduler = new RealScheduler((sch) => {
 
         sch.stop();
-        childTest.equal(sch.getSyntheticTimeElapsed(), 100);
-        childTest.end();
+        assert.equal(sch.getSyntheticTimeElapsed(), 100);
+        done();
     }, 100);
+    });
 });
 
-tap.test('1st call (with waitForTheFirstCall option false) returns syntheticTime equals to 0', function (childTest) {
-  var scheduler = new RealScheduler((sch) => {
+describe('RealScheduler', function() {
+    it('1st call (with waitForTheFirstCall option false) returns syntheticTime equals to 0', function(done) {
+        var scheduler = new RealScheduler((sch) => {
 
         sch.stop();
-        childTest.equal(sch.getSyntheticTimeElapsed(), 0);
-        childTest.equal(sch.getTimeElapsed(), 0);
-        childTest.end();
+        assert.equal(sch.getSyntheticTimeElapsed(), 0);
+        done();
     }, 100, {waitForTheFirstCall: false});
+    });
 });
-
-// tap.test('1st call (with waitForTheFirstCall option false) returns syntheticTime equals to 0', function (childTest) {
-//   var scheduler = new RealScheduler((sch) => {
-
-//         sch.stop();
-//         childTest.equal(sch.getSyntheticTimeElapsed(), 0);
-//         childTest.equal(sch.getTimeElapsed(), 0);
-//         childTest.end();
-//     }, 100, {waitForTheFirstCall: false});
-// });
-
 
