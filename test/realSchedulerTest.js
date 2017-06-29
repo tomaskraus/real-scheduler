66756a71eee433673b0f7c01ddc9e2aa5af4361c
tomaskraus <tomas.krausik@gmail.com>
2017-06-29 14:38:27 +0200

    all methods covered

--- test/realSchedulerTest.js
+++ test/realSchedulerTest.js
@@ -5,13 +5,26 @@ var assert = require('assert');
 var RealScheduler = require('../src/RealScheduler');
 
 var isStopCalled;
+var isDeltaCalled;
 
 var onStopHandler = function () {
     isStopCalled = true;
-}
+};
+var onDeltaHandler = function () {
+    isDeltaCalled = true;
+};
+
+var busyWait = function(delay) {
+    var startTime = new Date();
+    var currentTime = new Date();
+    while (currentTime - startTime < delay) {
+        currentTime = new Date();
+    }
+};
 
 beforeEach(function () {
     isStopCalled = false;
+    isDeltaCalled = false;
 });
 
 
@@ -21,6 +34,18 @@ describe('RealScheduler', function () {
 
             sch.stop();
             assert.equal(sch.getSyntheticTimeElapsed(), 100);
+            assert(sch.getTimeElapsed() > 0);
+            done();
+        }, 100);
+    });
+
+    it('1st call returns numberOfCalls equals to 1', function (done) {
+        var scheduler = new RealScheduler((sch) => {
+
+            sch.stop();
+            assert.equal(sch.getNumberOfCalls(), 1);
+            assert.equal(sch.getStatistics().numberOfCalls, 1);
+            assert(sch.getTimeElapsed() > 0);
             done();
         }, 100);
     });
@@ -41,6 +66,20 @@ describe('RealScheduler', function () {
             assert.equal(isStopCalled, true);
             done();
         }, 100, { onStop: onStopHandler });
+    });
+
+    it('Call onDeltaError handler if defined', function (done) {
+        var scheduler = new RealScheduler((sch) => {
+            if (sch.getNumberOfCalls() == 8) {
+                busyWait(500);
+            }
+            if (sch.getNumberOfCalls() == 10) {
+                sch.stop();
+                assert.equal(sch.getNumberOfCalls(), 10);
+                assert.equal(isDeltaCalled, true);
+                done();
+            }
+        }, 100, { onStop: onStopHandler, onDeltaError: onDeltaHandler });
     });
 });
 
